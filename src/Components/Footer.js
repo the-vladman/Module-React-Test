7a989207f10b55446fc15697c94c74ecc02088f8
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-08-12 10:22:55 -0500

    Footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Footer.css';
 
+import Content from './FooterContent';
+import Grid from '@material-ui/core/Grid';
+
 class Footer extends Component {
-    render() {
-        return (
-            <h1>Footer</h1>
-        );
-    }
+  render() {
+    return (
+      <Grid container justify="center">
+        <Grid item md={10}>
+          <Content></Content>
+        </Grid>
+    </Grid>
+    );
+  }
 }
 
 export default Footer;
