ae7b5995624c0ebcc71dd106bf90753e8a6a79d0
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-08-12 10:22:40 -0500

    alts warning

--- src/Components/FeatureItems.js
+++ src/Components/FeatureItems.js
@@ -4,28 +4,30 @@ import './FeatureItems.css';
 import Grid from '@material-ui/core/Grid';
 
 class FeatureItems extends Component {
-  FeatureItem(icon, title, description) {
+
+  featureItem(icon, title, description) {
     return (
       <Grid container spacing={8}>
         <Grid item md={2}>
-          <img id='feature-item-img' src={`${process.env.PUBLIC_URL}/imgs/${icon}`}></img>
+          <img id='feature-item-img' alt={icon} src={`${process.env.PUBLIC_URL}/imgs/${icon}`}></img>
         </Grid>
         <Grid id='feature-item-text' item md={10}>
           <p id='feature-item-title'>{title}</p>
           <p>{description}</p>
         </Grid>
     </Grid>
-  )}
+  )};
+
   render() {
     return (<Grid container>
       <Grid item md={4}>
-        {this.FeatureItem('icon1.png', 'Keep your Data', 'Enterprise-ready performance and controls')}
+        {this.featureItem('icon1.png', 'Keep your Data', 'Enterprise-ready performance and controls')}
       </Grid>
       <Grid item md={4}>
-        {this.FeatureItem('icon2.png', 'Prototype your Project', '24/7 proactive threat scanning to prevent attacks')}
+        {this.featureItem('icon2.png', 'Prototype your Project', '24/7 proactive threat scanning to prevent attacks')}
       </Grid>
       <Grid item md={4}>
-        {this.FeatureItem('icon3.png', 'Create Smart Texts', '24/7 proactive threat scanning to prevent attacks')}
+        {this.featureItem('icon3.png', 'Create Smart Texts', '24/7 proactive threat scanning to prevent attacks')}
       </Grid>
     </Grid>);
   }
