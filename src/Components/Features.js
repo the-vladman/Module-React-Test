6b97f04bc04f140f1159629ff8365039aabc9aec
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-08-17 16:59:44 -0500

    features responsive

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -3,18 +3,21 @@ import './Features.css';
 
 import Slider from './Slider';
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 
 class Features extends Component {
   render() {
     return (
       <div className='module-features' id='module-features-section'>
         <Grid container justify="center">
-          <Grid item md={10}>
+          <Grid item xs={11} sm={11} md={10}>
             <Grid container justify="flex-end">
-              <Grid item md={6}>
-                <img id='features-item-img' alt={`${process.env.PUBLIC_URL}/imgs/features.png`} src={`${process.env.PUBLIC_URL}/imgs/features.png`}></img>
-              </Grid>
-              <Grid item md={6}>
+              <Hidden only='xs'>
+                <Grid item sm={3} md={6}>
+                  <img id='features-item-img' alt={`${process.env.PUBLIC_URL}/imgs/features.png`} src={`${process.env.PUBLIC_URL}/imgs/features.png`}></img>
+                </Grid>
+              </Hidden>
+              <Grid item xs={12} sm={8} md={6}>
                 <Slider></Slider>
               </Grid>
             </Grid>
