26eacd12d1eb25fd2589b93e17787b0b2cbfc050
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-08-17 17:00:58 -0500

    landing responsive

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -5,22 +5,25 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import LandingItems from './LandingItems';
+import Hidden from '@material-ui/core/Hidden';
 
 class Landing extends Component {
     render() {
         return (
             <Grid container alignItems='center' justify="center">
-                <Grid item md={6}>
-                    <p id='landing-title'>The Best Way to Work Together</p>
-                    <p className='landing-description-text' id='landing-description'>We design and develop software for funded startups and</p>
-                    <p className='landing-description-text' id='landing-description-2'>established companies. Here are just some examples of our work</p>
-                </Grid>
-                <Grid item md={10}>
+                <Hidden only={['xs', 'sm']}>
+                    <Grid item xs={12} md={6}>
+                        <p id='landing-title'>The Best Way to Work Together</p>
+                        <p className='landing-description-text' id='landing-description'>We design and develop software for funded startups and</p>
+                        <p className='landing-description-text' id='landing-description-2'>established companies. Here are just some examples of our work</p>
+                    </Grid>
+                </Hidden>
+                <Grid item xs={12} md={10}>
                   <Card className='landing-banner'>
                     <CardMedia className='landing-banner-img' image={process.env.PUBLIC_URL + '/imgs/background.png'} title="Contemplative Reptile"></CardMedia>
                   </Card>
                 </Grid>
-                <Grid item md={10}>
+                <Grid item xs={11} md={10}>
                     <LandingItems></LandingItems>
                 </Grid>
             </Grid>
